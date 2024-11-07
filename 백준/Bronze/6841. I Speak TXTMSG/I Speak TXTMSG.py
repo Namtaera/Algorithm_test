c1dc3d334750640ce396a8eab426614749731c7a
남보라 <126854337+Namtaera@users.noreply.github.com>
2024-11-07 20:08:21 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.py
@@ -0,0 +1,40 @@
+while (True) :
+    n = str(input())
+    if n=="CU" :
+        print("see you")
+        
+    elif n==":-)" :
+        print("I’m happy")
+
+    elif n==":-(" :
+        print("I’m unhappy")
+
+    elif n==";-)" :
+        print("wink")
+
+    elif n==":-P" :
+        print("stick out my tongue")
+
+    elif n=="(~.~)" :
+        print("sleepy")
+
+    elif n=="TA" :
+        print("totally awesome")
+    
+    elif n=="CCC" :
+        print("Canadian Computing Competition")
+
+    elif n=="CUZ" :
+        print("because")
+
+    elif n=="TY" :
+        print("thank-you")
+
+    elif n=="YW" :
+        print("you’re welcome")
+    
+    elif n=="TTYL" :
+        print("talk to you later")
+        break
+    else :
+        print(n)
