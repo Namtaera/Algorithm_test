b6494b4283a93e7c0b3c0552443fe6e99829a0f0
남보라 <126854337+Namtaera@users.noreply.github.com>
2024-11-12 15:20:42 +0900

    출석 이벤트.py

--- 백준/Bronze/25704. 출석 이벤트/출석 이벤트.py
+++ 백준/Bronze/25704. 출석 이벤트/출석 이벤트.py
@@ -1,26 +1,30 @@
 n = int(input())
 p = int(input())
 sale = 0
+
 if n >= 5 and n < 10 :
     p = p-500
 
 elif n >= 10 and n < 15 :
     sale = p * 0.1
+    
     if sale < 500 :
         sale = 500
+        
     p = p-sale
 
 elif n >= 15 and n < 20 :
     sale = p * 0.1
+    
     if sale < 2000 :
         p = p - 2000
         
     else : 
         p = p - sale
         
-
 elif n >= 20 :
     sale = p * 0.25
+    
     if sale < 2000 :
         p = p-2000
 
