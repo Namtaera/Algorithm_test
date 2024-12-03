c2e71a20e07879a45d2edefc21f952706ab2dc7a
남보라 <126854337+Namtaera@users.noreply.github.com>
2024-12-03 19:56:03 +0900

    [Bronze II] Title: 분산처리, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,42 +1,26 @@
 T = int(input())
-
-two = [6,2,4,8]
-three = [1,3,9,7]
-four = [6,4]
-seven = [1,7,9,3]
-eight = [6,8,4,2]
-nine = [1,9]
-
 for j in range(T) :
     a, b = map(int, input().split())
-    a %= 10
+    a%=10
 
-    if a == 0:  
-        print(10)
-        
-    if a == 1 or a == 5 or a == 6 :
+    if a==1 or a==5 or a==6:
         print(a)
 
-    elif a == 2 :
-        res = b%4
-        print(two[res])
-        
-    elif a == 3 :
-        res = b%4
-        print(three[res])
-        
-    elif a == 7 :        
-        res = b%4
-        print(seven[res])
-        
-    elif a == 8 :
-        res = b%4
-        print(eight[res])
+    elif a==0:
+        print(10)
 
-    elif a == 4:
-        res = b%2    
-        print(four[res])
+    elif a==4 or a==9 :
+        b%=2
+        if b == 1:
+            print(a)
 
-    elif a == 9 :
-        res = b%2
-        print(nine[res])
+        else :
+            print((a**2)%10)
+
+    else :
+        b%=4
+        if b==0:
+            print((a**4)%10)
+
+        else :
+            print((a**b)%10)
